30a7e66fdbe1576640c38a98b512c83cd3e9fffd
AndreLp5 <aleppik7@gmail.com>
2024-06-10 13:44:25 +0300

    10.06.2024

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}","*.html"],
+  content: [],
   theme: {
     extend: {},
-    fontFamily: {
-      rosario: ["Rosario", "sans-serif"],
-    },
   },
   plugins: [],
 }
+
